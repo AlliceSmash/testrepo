9f667f11906780424ca10096b5579b4395141222
sabrina s <sabrinas@sabrinas-MacBook-Pro.local>
2019-12-26 20:44:51 -0500

    remove useless library

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
-import math
-import sys
-from os import rename
-
 import requests
 
 
 # print(sys.version)
-print(sys.executable)
+
+
 def greet(who_to_greet):
     greeting = "hello, {}".format(who_to_greet)
     return greeting
